a4f456ba1a30ab234894f2f09230142d985076d1
gabytal <45428645+gabytal@users.noreply.github.com>
2019-03-05 18:07:06 +0200

    ggg
    ggg

--- Tomcat-Final-Artifact/machine-content/port-modifier.sh
+++ Tomcat-Final-Artifact/machine-content/port-modifier.sh
@@ -4,7 +4,7 @@
 
 read -p "please choose a desired port: "  PORT
 
-sed -i "s|.*\<Connector port=\"8080\" protocol=\"HTTP\/1.1\"*|\<Connector port=\"${PORT}\" protocol=\"HTTP/1.1\"|g" /etc/tomcat/server.xml &&
+sed -i "s|.*\<Connector port=\"*\" protocol=\"HTTP\/1.1\"*|\<Connector port=\"${PORT}\" protocol=\"HTTP/1.1\"|g" /etc/tomcat/server.xml &&
 echo "the port successfully changed to $PORT!"
 sudo service tomcat restart
 sudo service tomcat7 restart
--- readme.md
+++
@@ -1,4 +0,0 @@
-#written by Gaby Tal
-
-#this is a full artifact of Tomcat7 machine using Vagrant, based on Ubuntu/Trusty box
-#for Lesson 2 HW
